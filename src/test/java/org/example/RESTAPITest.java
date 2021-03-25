3712e67f55294c178fd18e982c0e5a07660fb6df
ZORNICA <zornicag@yahoo.com>
2021-03-25 22:15:31 +0200

    this is commit from Bulgarian Timezone girl

--- src/test/java/org/example/RESTAPITest.java
+++ src/test/java/org/example/RESTAPITest.java
@@ -7,6 +7,8 @@ public class RESTAPITest {
     @Test
     public void postJira() {
         System.out.println("postJira");
+        System.out.println("postJira2");
+        System.out.println("postJira3");
     }
 
     @Test
